fde3b7db5ce13368b700b5af1d6234b2a057f0a7
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-06-27 18:43:31 -0400

    color coded time blocks using .each  and .prop  method, and this, along with moment js hour format , api.jquery.com momentjs.com, added id and class to associate the div class to time and color

--- challenge5_dayplanner/script.js
+++ challenge5_dayplanner/script.js
@@ -1,3 +1,26 @@
+//using momnent js to set current day
 var currentDay = moment().format("dddd, MMMM Do YYYY").toString();
 document.getElementById("currentDay").innerHTML = currentDay;
+
+
+//color coded time blocks using .each  and .prop  method, and this, along with moment js hour format , api.jquery.com
+var currentHour = moment().format("HH");
+const colorblock = document.getElementsByClassName("colorblock");
+
+
+$(".colorblock").each(function(){
+    var time = parseInt($(this).prop('id'));
+    if (time > currentHour){
+    $(this).css('background-color','#77dd77');
+    } else if (time < currentHour){
+    $(this).css('background-color','#d3d3d3');    
+    } else if (time === currentHour){
+    $(this).css('background-color','#ff6961');  
+    } else {
+        $(this).css('background-color','white');    
+    }
+});
+
+
+
 
